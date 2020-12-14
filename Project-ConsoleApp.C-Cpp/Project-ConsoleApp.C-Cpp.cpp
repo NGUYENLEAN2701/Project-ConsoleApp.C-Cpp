fe85b61bb63bc9eaf34aba674a07ab4a0c320116
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2020-12-14 18:38:51 +0700

    Add project files.

---
+++ Project-ConsoleApp.C-Cpp/Project-ConsoleApp.C-Cpp.cpp
@@ -0,0 +1,47 @@
+﻿#include "SolveQE.h"
+
+int main()
+{
+	//Cài đặt chương trình sử dụng tiếng Việt có dấu:
+	int setmode_IN = _setmode(_fileno(stdin), _O_U16TEXT);
+	int setmode_OUT = _setmode(_fileno(stdout), _O_U16TEXT);
+	SetConsoleTitleW(L"Giải Phương Trình Bậc Hai!");
+	HANDLE hdlConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_FONT_INFOEX consoleFont;
+	consoleFont.cbSize = sizeof(consoleFont);
+	GetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
+	memcpy(consoleFont.FaceName, L"consolas", sizeof(L"consolas"));
+	SetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
+	if (setmode_IN && setmode_OUT) system("cls");
+	//Chạy chương trình đến khi người dùng ngưng sử dụng
+	while (1)
+	{
+		//Nhập dữ liệu chương trình
+		std::wcout << L"Giải Phương Trình Bậc Hai!" << std::endl;
+		std::wcout << L"ax\u00B2 + bx + c = 0" << std::endl;
+		;
+		float a = Input_Value('a');
+		float b = Input_Value('b');
+		float c = Input_Value('c');
+		Print_Equation(a, b, c); //Hàm dùng để in ra màn hình phương trình đã nhập dữ liệu
+
+		//Giải chương trình
+		Result_QE x1, x2;
+		Solve_QuadraticEquation(a, b, c, &x1, &x2);
+		Print_Result_QE(x1, x2);
+
+		//Lựa chọn thoát hoặc tiếp tục chương trình
+		std::wcout << L"Ấn phím :\n"
+			<< L"->[ENTER] để tiếp tục chương trình...\n"
+			<< L"->[ESC] để thoát chương trình...";
+		do
+		{
+			wint_t Key_Exit = _getwch();
+			if (Key_Exit == ESC)
+				return 0;
+			else if (Key_Exit == ENTER)
+				break;
+		} while (1);
+		system("cls");
+	}
+}
---
+++ Project-ConsoleApp.C-Cpp/SolveQE.h
@@ -0,0 +1,229 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <string>
+#include <windows.h>
+#include <math.h>
+#include <algorithm>
+#include <conio.h>
+#include <sstream>
+#ifdef max
+#undef max
+#endif // max undef
+#ifdef ESC
+#undef ESC
+#endif
+#define ESC 27 // ESC
+#ifdef ENTER
+#undef ENTER
+#endif
+#define ENTER 13 // ENTER
+typedef struct Result_QE
+{
+	float Real_Value = 0;
+	float Virtual_Value = 0;
+	int Exist = -1; //-1: Vô Nghiệm; 0:Vô Số Nghiệm; 1:Nghiệm Thực; 2:Nghiệm Ảo;
+} Result_QE;
+
+float Input_Value(char c)
+{
+	float num = 0;
+	while (std::wcout << L"Nhập " << c << L" = " && !(std::wcin >> num))
+	{
+		std::wcin.clear();                                                   //clear bad input flag
+		std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+		std::wcout << L"Lỗi dữ liệu, thử lại!" << std::endl;
+	}
+	return num;
+}
+
+void Print_Equation(float a, float b, float c)
+{
+	std::wcout << L"PT: ";
+	//Print a:
+	if (a > 0)
+	{
+		if (a == 1)
+			std::wcout << L"x\u00B2 ";
+		else
+			std::wcout << a << L"x\u00B2 ";
+	}
+	else if (a < 0)
+	{
+		if (a == -1)
+			std::wcout << L"-x\u00B2 ";
+		else
+			std::wcout << a << L"x\u00B2 ";
+	}
+	//Print b:
+	if (b == 0)
+	{
+		if (a == 0)
+			std::wcout << L"0x ";
+	}
+	else if (b > 0)
+	{
+		if (b == 1)
+		{
+			if (a == 0)
+				std::wcout << L"x ";
+			else
+				std::wcout << L"+x ";
+		}
+		else
+		{
+			if (a == 0)
+				std::wcout << b << L"x ";
+			else
+				std::wcout << L"+" << b << L"x ";
+		}
+	}
+	else
+	{
+		if (b == -1)
+		{
+			std::wcout << L"-x ";
+		}
+		else
+		{
+			std::wcout << b << L"x ";
+		}
+	}
+	//Print c:
+	if (c > 0)
+	{
+		std::wcout << L"+" << c << L" ";
+	}
+	else if (c < 0)
+	{
+		std::wcout << c << L" ";
+	}
+	std::wcout << L"= 0" << std::endl;
+}
+
+void Solve_QuadraticEquation(float a, float b, float c, Result_QE* x1, Result_QE* x2)
+{
+	if (a == 0 && b == 0 && c == 0)
+	{
+		x1->Exist = 0;
+	}
+	else if (a == 0 && b == 0 && c != 0)
+		;
+	else if (a == 0)
+	{
+		x1->Exist = 1;
+		x1->Real_Value = -c / b;
+		if (!x1->Real_Value)
+			x1->Real_Value = 0;
+	}
+	else
+	{
+		float delta = b * b - 4 * a * c;
+		if (delta == 0)
+		{
+			x1->Exist = 1;
+			x1->Real_Value = -b / (2 * a);
+			if (!x1->Real_Value)
+				x1->Real_Value = 0;
+		}
+		else if (delta > 0)
+		{
+			x1->Exist = 1;
+			x1->Real_Value = (-b + sqrt(delta)) / (2 * a);
+			if (!x1->Real_Value)
+				x1->Real_Value = 0;
+
+			x2->Exist = 1;
+			x2->Real_Value = (-b - sqrt(delta)) / (2 * a);
+			if (!x2->Real_Value)
+				x2->Real_Value = 0;
+		}
+		else
+		{
+			x1->Exist = 2;
+			x1->Real_Value = (-b) / (2 * a);
+			x1->Virtual_Value = sqrt(-delta) / (2 * a);
+
+			x2->Exist = 2;
+			x2->Real_Value = (-b) / (2 * a);
+			x2->Virtual_Value = -sqrt(-delta) / (2 * a);
+		}
+	}
+}
+
+void Print_Value(Result_QE x, std::wstring s)
+{
+	switch (x.Exist)
+	{
+	case -1:
+	{
+
+		std::wcout << L"->Phương Trình Vô Nghiệm" << std::endl;
+		break;
+	}
+	case 0:
+	{
+		std::wcout << L"->Phương Trình Có Vô Số Nghiệm " << s << L"\u03f5Z" << std::endl;
+		break;
+	}
+	case 1:
+	{
+		std::wcout << L"-> " << s << L" = " << x.Real_Value << std::endl;
+		break;
+	}
+	case 2:
+	{
+		std::wcout << L"-> " << s << L" = ";
+		if (x.Real_Value)
+		{
+			std::wcout << x.Real_Value << L" ";
+			if (x.Virtual_Value > 0)
+			{
+				std::wcout << L"+";
+			}
+			else
+			{
+				std::wcout << L"";
+			}
+			if (x.Virtual_Value * x.Virtual_Value != 1)
+				std::wcout << x.Virtual_Value;
+			else if (x.Virtual_Value == -1)
+				std::wcout << L"-";
+		}
+		else
+		{
+			if (x.Virtual_Value * x.Virtual_Value != 1)
+				std::wcout << x.Virtual_Value;
+			else if (x.Virtual_Value == -1)
+				std::wcout << L"-";
+		}
+		std::wcout << L"i" << std::endl;
+		break;
+	}
+	default:
+	{
+		std::wcout << L"Error" << std::endl;
+	}
+	}
+}
+
+void Print_Result_QE(Result_QE x1, Result_QE x2)
+{
+	if (x2.Exist == -1)
+	{
+		Print_Value(x1, L"x");
+	}
+	else
+	{
+		if (x1.Real_Value <= x2.Real_Value)
+		{
+			Print_Value(x1, L"x\u2081");
+			Print_Value(x2, L"x\u2082");
+		}
+		else
+		{
+			Print_Value(x2, L"x\u2081");
+			Print_Value(x1, L"x\u2082");
+		}
+	}
+}
